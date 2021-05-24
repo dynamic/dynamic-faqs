f25d5c5aa8dda3d2b0918e7e9b5fb033a7d49633
Jason Irish <jsirish@users.noreply.github.com>
2021-05-24 16:46:40 -0500

    BUGFIX Various SS4 cleanup (#6)

---
+++ src/Controller/FAQPageController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Dynamic\FAQ\Page;
+
+use Dynamic\FAQ\Model\FAQ;
+use SilverStripe\Control\HTTPRequest;
+use SilverStripe\ORM\FieldType\DBHTMLText;
+use SilverStripe\View\ArrayData;
+
+class FAQPageController extends \PageController
+{
+    /**
+     * @var array
+     */
+    private static $allowed_actions = [
+        'view',
+    ];
+
+    /**
+     * @param HTTPRequest $request
+     * @return DBHTMLText
+     */
+    public function view(HTTPRequest $request)
+    {
+        $urlSegment = $request->latestParam('ID');
+
+        if (!$object = FAQ::get()->filter('URLSegment', $urlSegment)->first()) {
+            return $this->httpError(404, "The FAQ you're looking for doesn't seem to be here.");
+        }
+
+        return $this->customise(new ArrayData([
+            'Object' => $object,
+            'Title' => $object->Title,
+            'MetaTags' => $object->MetaTags(),
+            'Breadcrumbs' => $object->Breadcrumbs(),
+        ]))->renderWith([
+            'FAQ',
+            'Page',
+        ]);
+    }
+}
